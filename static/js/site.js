316ecef54c27f19ae94b5a450ba6e7d5904119a3
lorenries <lorenries@users.noreply.github.com>
2017-12-21 11:17:31 -0500

    add jquery again and a bunch of other stuff i guess?

--- static/js/site.js
+++ static/js/site.js
@@ -1,65 +1,64 @@
-$(document).ready( function() {
+document.addEventListener('DOMContentLoaded', function() {
+	feather.replace();
 
-  feather.replace();
+	var mobileMenuButton = document.querySelector('.js-menu-button');
+	var mobileMenu = document.querySelector('.mobile-menu');
+	var body = document.querySelector('body');
 
-  var $mobileMenuButton = $('.js-menu-button');
-  var $mobileMenu = $('.js-menu-mobile');
-  var $body = $('body');
+	mobileMenuButton.addEventListener('click', function(event) {
+		event.preventDefault();
+		console.log('clicked');
+		if (mobileMenu.classList.contains('open')) {
+			toggleMenuClose();
+		} else {
+			toggleMenuOpen();
+		}
+	})
 
-  $mobileMenuButton.unbind('click').click(function(event) {
-  	event.preventDefault();
-  	if ($mobileMenuButton.hasClass('open')) {
-  		toggleMenuClose();
-  	} else {
-  		toggleMenuOpen();
-  	}
-  })
+	function toggleMenuOpen() {
+		mobileMenu.classList.remove('closed');
+		mobileMenu.classList.add('open');
+		mobileMenuButton.innerHTML = '<i data-feather="x" class="w2 h-auto pa1"></i>';
+		feather.replace();
+		body.classList.add('prevent-scroll');
+	}
 
-  function toggleMenuOpen() {
-  	$mobileMenu.removeClass('dn');
-  	$mobileMenu.addClass('flex');
-  	$mobileMenu.animate({
-  		height: '100vh',
-  		top: '0'
-  	}, 800, 'swing', function() {
-  		$mobileMenuButton.html('<i data-feather="x" class="w2 h-auto pa1"></i>');
-  		feather.replace();
-  	});
-  	$body.addClass('prevent-scroll');
-  	$mobileMenuButton.addClass('open');
-  }
+	function toggleMenuClose() {
+		mobileMenu.classList.remove('open');
+		mobileMenu.classList.add('closed');
+		mobileMenuButton.innerHTML = '<i data-feather="menu" class="w2 h-auto pa1"></i>';
+		feather.replace();
+		body.classList.remove('prevent-scroll');
+	}
 
-  function toggleMenuClose() {
-  	$mobileMenu.animate({
-  		height: '0px',
-  		top: '-100%'
-  	}, 500, 'swing', function() {
-  		$mobileMenuButton.html('<i data-feather="menu" class="w2 h-auto pa1"></i>');
-  		feather.replace();
-  		$mobileMenu.addClass('dn');
-  		$mobileMenu.removeClass('flex');
-  	});
-  	$body.removeClass('prevent-scroll');
-  	$mobileMenuButton.removeClass('open');
-  }
+	var isotopeGrid = document.querySelector('.isotope-grid');
+	
+	imagesLoaded(isotopeGrid, initIsotope);
 
-  var $isotopGrid = $('.isotope-grid').imagesLoaded( function() {
-    // init Isotope after all images have loaded
-    $isotopGrid.isotope({
-      // options
-      layoutMode: 'fitRows'
-    });
-  });
+	function initIsotope() {
+		
+		var iso = new Isotope(isotopeGrid, {
+			layoutMode: 'fitRows'
+		});	
 
-  $('.js-filter-group').on( 'click', 'a', function(event) {
-  	event.preventDefault();
-    var filterValue = $(this).attr('data-filter');
-    $isotopGrid.isotope({ filter: '.' + filterValue });
-    $(this).parent().parent().children().children().removeClass('menu-selected');
-    if (!$(this).parent().parent().children().children().hasClass('menu-border-animate')) {
-    	$(this).parent().parent().children().children().addClass('menu-border-animate')
-    }
-    $(this).addClass('menu-selected');
-  });
+		var jsFilterGroup = document.querySelector('.js-filter-group');
 
-});
+		jsFilterGroup.addEventListener('click', function(event) {
+			
+			event.preventDefault();
+
+			if (event.target.nodeName === 'A') {
+				var filterValue = event.target.getAttribute('data-filter');
+				iso.arrange({ filter: '.' + filterValue });
+				for(var i = 0; i < this.children.length; i++) {
+					this.children[i].firstChild.classList.remove('menu-selected');
+				}
+				if (!event.target.classList.contains('menu-border-animate')) {
+					event.target.classList.add('menu-border-animate')
+				}
+				event.target.classList.add('menu-selected');
+			}
+
+		});
+	}
+}, false);
